966390b616daefb576b2150ed2abfeeb4fb7513b
David Annopolsky <dannopolsky@freepeople.com>
2014-08-06 10:31:51 -0400

    described the methods and constructor, refactored for modern browsers

--- utils/request.js
+++ utils/request.js
@@ -1,5 +1,9 @@
+// Forked from https://github.com/krasimir/absurd
+// Refactored to support only modern browsers and IE9+
 'use strict';
 
+// Helper methods
+// underscore prefix represents private methods
 var _hasOwn = function(obj, prop) {
     return Object.prototype.hasOwnProperty.call(obj, prop);
 };
@@ -24,41 +28,56 @@ var _setHeaders = function(headers) {
         }
     }
 };
+/**
+ * Constructor for MicroRequest
+ * sets up config and runs the process method
+ * when a new instance is created
+ * @param {Object} config [pass in criteria to xhr]
+ */
 var MicroRequest = function(config) {
     if (typeof config !== 'object') throw new TypeError('"' + config + '" Must be an object');
     this.config = {
         url: config.url || '',
         type: config.type || 'get',
         data: config.data || {},
-        headers: config.headers || ''
+        headers: config.headers || '',
+        json: config.json || false
     };
     this.process();
 };
 var MicroRequestProto = MicroRequest.prototype;
 
+/**
+ * Process method is the meat and potatoes
+ * handles the xhr request and funnels the appropriate responses
+ * @return {Object} [the object itself meant for chaining methods]
+ */
 MicroRequestProto.process = function() {
     this.xhr = new XMLHttpRequest();
-    this.xhr.onreadystatechange = function() {
+    this.xhr.onload = function() {
         var result;
         var error;
         var status = this.xhr.status;
         var statusText = this.xhr.statusText;
-        if (this.xhr.readyState === 4) {
-            if (status >= 200 && status < 300 || status === 304) {
-                result = this.xhr.responseText;
-                if (this.config.json === true && typeof JSON !== 'undefined') {
-                    result = JSON.parse(result);
-                }
-                this.doneCallback(result, this.xhr, statusText);
-            } else {
-                error = this.xhr.response;
-                this.failCallback(error, this.xhr, statusText);
+        if (status >= 200 && status < 300 || status === 304) {
+            result = this.xhr.responseText;
+            if (this.config.json === true) {
+                result = JSON.parse(result);
             }
-            this.alwaysCallback(statusText);
+            this.doneCallback && this.doneCallback(result, this.xhr, statusText);
+        } else {
+            error = this.xhr.response;
+            this.failCallback && this.failCallback(error, this.xhr, statusText);
         }
+        return this.alwaysCallback && this.alwaysCallback(statusText);
+
     }.bind(this);
 
-    if (this.config.type.toLowerCase() === 'get') {
+    this.xhr.onerror = function() {
+        throw new Error('Can\'t Connect');
+    };
+
+    if (this.config.type === 'get' || this.config.type === 'GET') {
         this.xhr.open("GET", this.config.url +
             _getParams.call(this, this.config.data, this.config.url), true);
         this.xhr.send();
@@ -76,19 +95,37 @@ MicroRequestProto.process = function() {
 
     return this;
 };
-
+/**
+ * Done method gets called when xhr has
+ * been processed successfully.
+ * @param  {Function} callback [returns data, xhr, and status]
+ * @return {Object}          [returns the object for chaining]
+ */
 MicroRequestProto.done = function(callback) {
     this.doneCallback = callback;
     return this;
 };
-
+/**
+ * Fail occurs when the xhr responds with an
+ * error such as a 404 or similar
+ * @param  {Function} callback [returns error, xhr, and status]
+ * @return {Object}            [returns the object for chaining]
+ */
 MicroRequestProto.fail = function(callback) {
     this.failCallback = callback;
     return this;
 };
 
+/**
+ * Always method happens regardless or xhr responses
+ * so with that, you can do something on success(done)
+ * or on fail
+ * @param  {Function} callback [returns status]
+ * @return {[type]}            [returns the object for chaining]
+ */
 MicroRequestProto.always = function(callback) {
     this.alwaysCallback = callback;
     return this;
 };
+
 module.exports = MicroRequest;
